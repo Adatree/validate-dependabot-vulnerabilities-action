1398b2922930cfcfb28673672151504c7bd95287
Leo Alves <leo@adatree.com.au>
2023-01-16 11:02:54 +1000

    ADA-2555: attempt to use GITHUB_TOKEN

--- src/validateDependencies.js
+++ src/validateDependencies.js
@@ -4,7 +4,7 @@ const errorMessage = 'Build contains vulnerabilities that violate Adatree\'s inf
 const millisecondsInOneDay = 24 * 60 * 60 * 1000
 
 const validateDependencies = async () => {
-    const auth = process.env.GITHUB_PAT
+    const auth = process.env.GITHUB_TOKEN
     const owner = 'Adatree'
     const openState = 'open'
     const octokit = new Octokit({auth})
